790dc7e06cbad6f09f0ecdb35bfcfb0bb1b50bae
Прораб <@SP.int>
2023-02-03 16:49:12 +0300

    feat: add nextQues

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const userData = localStorage.getItem('user');
+const URI_DATA = './db/data.json';
 
 if (!userData) {
   window.location.href = './login';
@@ -7,5 +8,46 @@ if (!userData) {
 const user = JSON.parse(userData);
 
 const userNameElem = document.querySelector('#userName');
+const quesElem = document.querySelector('#ques');
+const answersElem = document.querySelector('#answers');
+let countQues = 0;
+let curIndQues = 0;
 
 userNameElem.innerHTML = user.name;
+
+window.onload = async function () {
+  const data = await loadData(URI_DATA);
+
+  console.log(data);
+
+  nextQues(data);
+};
+
+const loadData = async (URI) => {
+  const response = await fetch(URI, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'GET',
+  });
+
+  return await response.json();
+};
+
+const nextQues = (data) => {
+  quesElem.innerHTML = data[curIndQues].q;
+
+  const ansElem = data[curIndQues].ans
+    .map((ans, i) => {
+      if (i === 0) {
+        return `<div class="answer" right>${ans}</div>`;
+      } else {
+        return `<div class="answer">${ans}</div>`;
+      }
+    })
+    .sort(() => (Math.random() > 0.5 ? 1 : -1));
+  answersElem.innerHTML = ansElem.join('');
+
+  curIndQues += 1;
+};
